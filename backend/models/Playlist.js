99015cffb69e5e133889ae3057366831ffa07bd8
unknown <anshavdesai@gmail.com>
2025-03-22 14:38:13 +0530

    New 2 Commit

--- backend/models/Playlist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PlaylistSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    songs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }]
-});
-
-module.exports = mongoose.model('Playlist', PlaylistSchema);
