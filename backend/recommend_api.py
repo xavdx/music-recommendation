86f46835704e6b0475bda2e6355e3aba4dffe633
unknown <anshavdesai@gmail.com>
2025-04-07 19:33:23 +0530

    Adding new color for get recommendations button hover 2

--- backend/recommend_api.py
+++ backend/recommend_api.py
@@ -193,6 +193,7 @@ def get_all_users():
     collections = list(collections_collection.find({}, {'_id': 0}))
     history = list(history_collection.find({}, {'_id': 0}))
     return jsonify({'users': users, 'collections': collections, 'history': history})
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
