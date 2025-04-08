8d4123853cc9882491b7f422ae0cd3a18cf0b931
unknown <anshavdesai@gmail.com>
2025-04-08 22:44:01 +0530

    Adding the logo to the main login page 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
                 <img 
                     src="/logo4.png" 
                     alt="Music Recommendation Logo" 
-                    className="w-24 h-24 mb-0" //size
+                    className="w-30 h-30 mb-5" //size
                 />
                 <h1 className="text-4xl font-bold text-card mb-6">
                     Music Recommendation Engine
